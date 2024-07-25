e0db16b11144d78e8d186b82230c9685f0a5016c
Rodolfo Chacon <rudychacon01@gmail.com>
2024-07-25 17:13:53 -0600

    Creacion clase SuperHeroe

--- EjercicioClase/Program.cs
+++ EjercicioClase/Program.cs
@@ -1,5 +1,21 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+using EjercicioClase;
+
+Console.WriteLine("********Superheroes********");
 
 
 
-Console.WriteLine("Hello, World!");
+
+SuperHeroe superHeroe1 = new SuperHeroe();
+superHeroe1.Nombre = "Batman";
+superHeroe1.identidadSecreta = "identidadSecreta";
+superHeroe1.ciudad = "ciudad";
+superHeroe1.puedeVolar = "puedeVolar";
+superHeroe1.superPoder = "superPoder";
+
+
+Console.WriteLine(superHeroe1.Nombre);
+Console.WriteLine(superHeroe1.identidadSecreta);
+Console.WriteLine(superHeroe1.ciudad);
+Console.WriteLine(superHeroe1.puedeVolar);
+Console.WriteLine(superHeroe1.superPoder);
---
+++ EjercicioClase/SuperHeroe.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+
+
+//Crear una clase llamada SuperHeroe la cual tendrá los siguientes atributos:
+//nombre
+//identidadSecreta
+//ciudad
+//puedeVolar
+//superPoder (Objeto de la clase SuperPoder)
+
+
+
+
+
+namespace EjercicioClase
+{
+    public class SuperHeroe
+    {
+        public string Nombre { get; set; }
+        public string identidadSecreta { get; set; }
+        public string ciudad { get; set; }
+        public string puedeVolar { get; set; }
+        public string superPoder { get; set; }
+
+
+    }
+}
