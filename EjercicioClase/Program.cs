8edf911853de517d3ace2aee73daec5c040f29d9
Rodolfo Chacon <rudychacon01@gmail.com>
2024-07-25 18:51:59 -0600

    avance

--- EjercicioClase/Program.cs
+++ EjercicioClase/Program.cs
@@ -8,7 +8,9 @@ Console.WriteLine("");
 
 
 
-//Instancia para SuperHeroe #1
+//Instancia para SuperHeroe de prueba 
+Console.WriteLine("Ingrese los datos del superheroe como el en siguiente ejemplo: ");
+Console.WriteLine("");
 SuperHeroe superHeroe1 = new SuperHeroe();
 superHeroe1.Nombre = "Batman";
 superHeroe1.IdentidadSecreta = "Bruce Wayne";
@@ -16,7 +18,7 @@ superHeroe1.Ciudad = "Gotham";
 superHeroe1.PuedeVolar = "puedeVolar";
 
 
-//Instancia para SuperPoder 
+//Instancia para SuperPoder prueba 
 superPoder superPoder1 = new superPoder();
 superPoder1.Snombre = "Inteligencia Superior";
 superPoder1.Sdescripcion = "Habilidad en el Combate Cuerpo a Cuerpo";
@@ -25,10 +27,14 @@ superPoder1.Snivel = "6";
 
 superHeroe1.imprimir();
 superPoder1.imprimir();
+Console.WriteLine("");
+
+
+Console.WriteLine("*******Inicie aqui*******");
 
 
 
-//Instancia para SuperHeroe #2
+//Instancia para SuperHeroe #1
 SuperHeroe superHeroe2 = new SuperHeroe();
 Console.Write("Ingrese el nombre del superheroe: ");
 superHeroe2.Nombre = Console.ReadLine();
@@ -40,7 +46,7 @@ Console.Write("Tiene habilidad para volar? ");
 superHeroe2.PuedeVolar = Console.ReadLine();
 
 
-//Instancia para SuperPoder#2
+//Instancia para SuperPoder#1
 superPoder superPoder2 = new superPoder();
 Console.WriteLine("Ingrese el nombre del super poder: ");
 superPoder2.Snombre = Console.ReadLine();
@@ -54,3 +60,48 @@ Console.WriteLine("");
 
 superHeroe2.imprimir();
 superPoder2.imprimir();
+
+
+Console.WriteLine("");
+Console.WriteLine("*******");
+Console.WriteLine("");
+
+
+
+
+
+
+
+
+
+
+//Instancia para SuperHeroe #2
+SuperHeroe superHeroe3 = new SuperHeroe();
+Console.Write("Ingrese el nombre del superheroe: ");
+superHeroe3.Nombre = Console.ReadLine();
+Console.Write("Cual es su identidad secreta? ");
+superHeroe3.IdentidadSecreta = Console.ReadLine();
+Console.Write("Ingrese su ciudad ");
+superHeroe3.Ciudad = Console.ReadLine();
+Console.Write("Tiene habilidad para volar? ");
+superHeroe3.PuedeVolar = Console.ReadLine();
+
+
+
+
+
+
+//Instancia para SuperPoder#2
+superPoder superPoder3 = new superPoder();
+Console.WriteLine("Ingrese el nombre del super poder: ");
+superPoder3.Snombre = Console.ReadLine();
+Console.WriteLine("Descripcion del poder: ");
+superPoder3.Sdescripcion = Console.ReadLine();
+Console.WriteLine("Nivel de poder");
+superPoder3.Snivel = Console.ReadLine();
+
+Console.WriteLine("");
+
+
+superHeroe3.imprimir();
+superPoder3.imprimir();
