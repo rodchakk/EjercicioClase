b7289d8a3d27380c31faffa47ce48fd9caaf3a8d
Rodolfo Chacon <rudychacon01@gmail.com>
2024-07-25 18:43:14 -0600

    Avance

--- EjercicioClase/Program.cs
+++ EjercicioClase/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine("");
 
 
 
-//Instancia para SuperHeroe
+//Instancia para SuperHeroe #1
 SuperHeroe superHeroe1 = new SuperHeroe();
 superHeroe1.Nombre = "Batman";
 superHeroe1.IdentidadSecreta = "Bruce Wayne";
@@ -28,3 +28,29 @@ superPoder1.imprimir();
 
 
 
+//Instancia para SuperHeroe #2
+SuperHeroe superHeroe2 = new SuperHeroe();
+Console.Write("Ingrese el nombre del superheroe: ");
+superHeroe2.Nombre = Console.ReadLine();
+Console.Write("Cual es su identidad secreta? ");
+superHeroe2.IdentidadSecreta = Console.ReadLine();
+Console.Write("Ingrese su ciudad ");
+superHeroe2.Ciudad = Console.ReadLine();
+Console.Write("Tiene habilidad para volar? ");
+superHeroe2.PuedeVolar = Console.ReadLine();
+
+
+//Instancia para SuperPoder#2
+superPoder superPoder2 = new superPoder();
+Console.WriteLine("Ingrese el nombre del super poder: ");
+superPoder2.Snombre = Console.ReadLine();
+Console.WriteLine("Descripcion del poder: ");
+superPoder2.Sdescripcion = Console.ReadLine();
+Console.WriteLine("Nivel de poder");
+superPoder2.Snivel = Console.ReadLine();
+
+Console.WriteLine("");
+
+
+superHeroe2.imprimir();
+superPoder2.imprimir();
